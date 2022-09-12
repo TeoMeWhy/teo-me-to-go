f88ab42198551cbac55a26f082644e3f906202e7
Teo Calvo <teocalvo2@gmail.com>
2022-09-12 09:14:12 -0300

    Capítulo 02, exercício 03

---
+++ capitulo_02/exercicios/ex_03.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(`Ex 02: Embora seja excessivamente qualificado para a tarefa, podemos usar Go como uma calculadora.
+Escreva um programa que calcule 32132 x 42452 e exiba o resultado no terminal (utilize o operador * para a multiplicação`)
+
+	fmt.Println("32132 x 42452 =", 32132*42452)
+
+}
