139bebe750b46fdab584e69890938f554d7606dd
Teo Calvo <teocalvo2@gmail.com>
2022-09-12 09:17:44 -0300

    Capítulo 02, exercício 04

--- capitulo_02/exercicios/ex_03.go
+++ capitulo_02/exercicios/ex_03.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 
-	fmt.Println(`Ex 02: Embora seja excessivamente qualificado para a tarefa, podemos usar Go como uma calculadora.
+	fmt.Println(`Ex 03: Embora seja excessivamente qualificado para a tarefa, podemos usar Go como uma calculadora.
 Escreva um programa que calcule 32132 x 42452 e exiba o resultado no terminal (utilize o operador * para a multiplicação`)
 
 	fmt.Println("32132 x 42452 =", 32132*42452)
---
+++ capitulo_02/exercicios/ex_04.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(`Ex 04: O que é uma string? Como descobrimos seu tamanho?`)
+
+	fmt.Println(`
+As strings são sequência de caracteres, onde cada um é representado por um ou mais bytes.	
+Pode-se obter o tamanho de uma string invocando a função len(): len("Olá mundo").`)
+
+}
