3a74c7f5f1801f2f131a121f43b20eeb8c34cbbf
Teo Calvo <teocalvo2@gmail.com>
2022-09-12 09:02:24 -0300

    Capítulo 02, exercício 01

---
+++ capitulo_02/exercicios/ex_01.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Ex 01: Como os inteiros são armazenados em um computador?")
+
+	fmt.Println("\nResposta: O inteiros são armazenados em formato binário pelo computador, isso é, sequência de 0 (zeros) e 1 (uns).")
+
+}
